46fb12155c9500c06d958fc2b63248655316c255
techplayz32 <154914363+techplayz32@users.noreply.github.com>
2025-06-20 23:30:15 +0300

    chore(config): added merge to the type-enum and added scope-enum, as well fixed husky

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,26 +1,37 @@
+/** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
-    extends: [
-      '@commitlint/config-conventional',
-    ],
-    rules: {
-      'type-enum': [
-        2, // rules lvls: 0-off, 1-warn, 2-error
-        'always', // something
-        [ // value
-          'build',
-          'chore',
-          'ci',
-          'docs',
-          'feat',
-          'fix',
-          'perf',
-          'refactor',
-          'revert',
-          'style',
-          'test',
-          'merge',
-        ],
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "merge", // custom
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
       ],
-    },
-  };
-  
+    ],
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "auth", 
+        "ui", 
+        "api", 
+        "deps", 
+        "config", 
+        "github"
+      ],
+    ],
+    // 'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+  },
+};
